0683f1ffa22df14e307de882afee5e09e0441851
Beirun <orillobernardjay@gmail.com>
2025-10-10 12:48:07 +0800

    feat: add api route for user logout

---
+++ app/api/auth/logout/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req : NextRequest) {
+    
+    const token = req.cookies.get('refreshToken');
+
+    const res = NextResponse.json({ message: "Logged out successfully" });
+    if(token){
+        res.cookies.delete('resfreshToken')
+    }
+    
+  return res;
+}
