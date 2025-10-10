e0b8f2aa894c7eed5f211143726328abb166490f
Beirun <orillobernardjay@gmail.com>
2025-10-11 01:59:56 +0800

    Update user schema

--- schema/user.ts
+++ schema/user.ts
@@ -6,7 +6,7 @@ export const user = pgTable('user', {
   roleId: integer('role_id').references(() => role.roleId).notNull(),
   firstName: text('first_name').notNull(),
   lastName: text('last_name').notNull(),
-  userPhone: text('user_phone').unique().notNull(),
+  phoneNumber: text('phone_number').unique().notNull(),
   passwordHash: text('password_hash').notNull(),
   isVerified: boolean('is_verified').notNull().default(false),
   dateCreated: timestamp('date_created', { withTimezone: true }).notNull(),
