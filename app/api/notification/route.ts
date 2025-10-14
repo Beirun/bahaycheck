ceb30c3e71d33b8e09415952e1dfa48c8cc24186
Beirun <orillobernardjay@gmail.com>
2025-10-14 17:26:25 +0800

    Add notification main route

---
+++ app/api/notification/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db/drizzle";
+import { notification } from "@/schema/notification";
+import { eq } from "drizzle-orm";
+import { authenticateToken } from "@/utils/auth";
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { notificationId } = await req.json(); 
+    if (isNaN(notificationId)) return NextResponse.json({ error: "Invalid notification ID" }, { status: 400 });
+
+    const now = new Date();
+
+    const updated = await db
+      .update(notification)
+      .set({ dateUpdated: now, dateDeleted: now })
+      .where(eq(notification.notificationId, notificationId))
+      .returning();
+
+    if (updated.length === 0)
+      return NextResponse.json({ error: "Notification not found" }, { status: 404 });
+
+    return NextResponse.json({ message: "Notification updated", data: updated[0] });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+
+export async function GET(req: NextRequest){
+  try {
+    const { userId } = await authenticateToken(req);
+    if (isNaN(userId!)) return NextResponse.json({ error: "Invalid user ID" }, { status: 400 });
+
+    const data = await db.select().from(notification).where(eq(notification.userId, userId!));
+    return NextResponse.json({notifications: data});
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
