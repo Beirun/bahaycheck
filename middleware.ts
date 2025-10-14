aa66faa39f0e123f9880ab7ff375912616b0230f
Beirun <orillobernardjay@gmail.com>
2025-10-14 17:26:23 +0800

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,35 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authenticateToken } from "@/utils/auth";
-export async function middleware(req: AuthRequest) {
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  if (pathname.startsWith("/api/admin")) {
-    await authenticateToken(req);
-    if (req.user.role.toLowerCase() !== "admin") {
-      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  const authenticatedRoutes = ["/api/admin", "/api/user", "/api/volunteer","/api/notification"];
+  if (authenticatedRoutes.some((route) => route.includes(pathname))) {
+    const payload = await authenticateToken(req);
+    if (payload.error)  return payload.error;
+    
+    const role = payload.role;
+    if (pathname.startsWith("/api/admin")) {
+      if (role.toLowerCase() !== "admin") {
+        return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+      }
+      return NextResponse.next();
+    }
+    if (pathname.startsWith("/api/volunteer")) {
+      if (role.toLowerCase() !== "volunteer") {
+        return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+      }
+      return NextResponse.next();
+    }
+    if (pathname.startsWith("/api/user")) {
+      if (role.toLowerCase() !== "user") {
+        return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+      }
+      return NextResponse.next();
     }
     return NextResponse.next();
   }
-  if (pathname.startsWith("/api/volunteer")) {
-    await authenticateToken(req);
-    if (req.user.role.toLowerCase() !== "volunteer") {
-      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-    }
-    return NextResponse.next();
-  }
-  if (pathname.startsWith("/api/user")) {
-    await authenticateToken(req);
-    if (req.user.role.toLowerCase() !== "user") {
-      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-    }
-    return NextResponse.next();
-  }
-  return NextResponse.next();
 }
+
 export const config = {
-  matcher: ["/api/admin/:path*", "/api/volunteer/:path*", "/api/user/:path*"],
+  matcher: "/api/:path*", // Apply to all API routes and other paths, excluding static assets
 };
