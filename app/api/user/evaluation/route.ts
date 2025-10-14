61265a0b400470a10927db547c3773b5c27deecc
Beirun <orillobernardjay@gmail.com>
2025-10-14 17:26:26 +0800

    Add user API routes

---
+++ app/api/user/evaluation/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db/drizzle";
+import { evaluation } from "@/schema/evaluation";
+import { request } from "@/schema/request";
+import { houseCategory } from "@/schema/houseCategory";
+import { damageCategory } from "@/schema/damageCategory";
+import { and, eq, isNull } from "drizzle-orm";
+import { authenticateToken } from "@/utils/auth";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// api/admin/evaluation
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await authenticateToken(req);
+    const records = await db
+      .select({
+        evaluationId: evaluation.evaluationId,
+        note: evaluation.note,
+        dateCreated: evaluation.dateCreated,
+        dateUpdated: evaluation.dateUpdated,
+        requestId: request.requestId,
+        houseCategory: houseCategory.houseCategoryName,
+        damageCategory: damageCategory.damageCategoryName,
+      })
+      .from(evaluation)
+      .innerJoin(request, eq(evaluation.requestId, request.requestId))
+      .innerJoin(houseCategory, eq(evaluation.houseCategoryId, houseCategory.houseCategoryId))
+      .innerJoin(damageCategory, eq(evaluation.damageCategoryId, damageCategory.damageCategoryId))
+      .where(and(isNull(evaluation.dateDeleted),eq(request.userId,userId!)))
+      .orderBy(evaluation.evaluationId);
+
+    return NextResponse.json(records);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
