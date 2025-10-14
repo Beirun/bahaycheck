9f2717cc8a129c7e6806ef0b8d7abe56588ae9c0
Beirun <orillobernardjay@gmail.com>
2025-10-14 17:26:26 +0800

    Add volunteer license API routes

---
+++ app/api/volunteer/license/route.ts
@@ -0,0 +1,27 @@
+import {  NextRequest, NextResponse } from "next/server";
+import { db } from "@/db/drizzle";
+import { license } from "@/schema/license";
+import { eq } from "drizzle-orm";
+import { authenticateToken } from "@/utils/auth";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// api/volunteer/license
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await authenticateToken(req);
+    const record = await db
+      .select()
+      .from(license)
+      .where(eq(license.userId,userId!)).limit(1);
+
+    return NextResponse.json(record);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
